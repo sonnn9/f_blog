70a2a6cd18cfe9cc0b02b6337993eea02d2949b9
khanhdq123 <khanhdq@fabbi.io>
2019-11-26 16:15:30 +0700

    Creat seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(userSeeder::class);
+
+    }
+}
+
+class userSeeder extends Seeder{
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        DB::table('users')->insert([
+                ['name'=>'khanhdq123', 'email' => 'khanhdq123@fabii.io',  'password' => bcrypt('123456'), 'level' => 1],
+                ['name'=>'minhlq', 'email' => 'minhlq@fabii.io',  'password' => bcrypt('123456'), 'level' => 0],
+                ['name'=>'tanlv', 'email' => 'tanlv@fabii.io',  'password' => bcrypt('123456'), 'level' => 0],
+                ['name'=>'sonnn', 'email' => 'sonnn@fabii.io',  'password' => bcrypt('123456'), 'level' => 0],
+                ['name'=>'cuonglv', 'email' => 'cuonglv@fabii.io',  'password' => bcrypt('123456'), 'level' => 1]
+            ]);
+
     }
 }
