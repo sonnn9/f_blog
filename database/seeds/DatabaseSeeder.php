ce77083fcfc38317b9f051799eee66ba9e365f56
phuonght-fabbi <phuonght@fabbi.io>
2019-11-27 08:52:25 +0700

    seeder categories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(userSeeder::class);
+        $this->call([
+            userSeeder::class,
+            CategoriesTableSeeder::class,
+        ]);
 
     }
 }
