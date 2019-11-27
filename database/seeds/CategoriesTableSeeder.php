a050dbb027975896f039194403bc207c5d720d5f
phuonght-fabbi <phuonght@fabbi.io>
2019-11-27 10:38:25 +0700

    seeder categories

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,9 +12,9 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
-            ['name'=>'Tình cảm'],
-            ['name'=>'Kinh dị'],
-            ['name'=>'Hài hước'],            
+            ['name' => 'Tình cảm'],
+            ['name' => 'Kinh dị'],
+            ['name' => 'Hài hước'],            
         ]);
     }
 }
